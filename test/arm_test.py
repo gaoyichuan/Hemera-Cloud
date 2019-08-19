b77571f4258ae6d4fe2cdccbe16b8d8d57018ddf
gaoyichuan <gaoyichuan000@gmail.com>
2019-08-19 13:07:02 +0800

    Arm: Add test for GET_STATUS

--- test/arm_test.py
+++ test/arm_test.py
@@ -1,7 +1,8 @@
 from aiohttp import web
 import socketio
+import json
 
-sio = socketio.AsyncServer()
+sio = socketio.AsyncServer(async_mode='aiohttp', async_handlers=True)
 app = web.Application()
 sio.attach(app)
 
@@ -18,7 +19,39 @@ async def connect(sid, environ):
 @sio.on('BIT_STREAM_RESULT')
 async def bitstream_handle(sid, data):
     print('bitstream result: sid={}, success={}'.format(sid, data['success']))
+    await sio.emit('GET_STATUS')
+    
+
+@sio.on('LED_SWITCH_STATUS')
+async def led_status_handle(sid, data):
+    print('led status result: sid={}, leds={}'.format(sid, data['leds']))
+
+
+class TestApp(object):
+    def __init__(self):
+        self.thread = None
+
+    def start_server(self):
+        print('starting server')
+        web.run_app(app, port=3412)
+
+    def start(self):
+        self.thread = sio.start_background_task(self.start_server)
+
+    def send_msg(self, event, data):
+        print('emit msg {}'.format(event))
+        sio.emit(event, json.dumps(data))
+
+    def wait(self):
+        self.thread.join()
 
 
 if __name__ == '__main__':
-    web.run_app(app, port=3412)
+    testapp = TestApp()
+    testapp.start()
+
+    print("wtf")
+
+    while True:
+        sio.sleep(1)
+        testapp.send_msg('GET_STATUS', '')
